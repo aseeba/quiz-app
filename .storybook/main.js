b242f4de2a2b61dfe655ee25b0755bab2947b2eb
Anja Seeba <ahoi@achtsam.media>
2021-01-21 13:52:35 +0100

    Add changes

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+import '../styles/index.css'
+
+import '../components/app-grid.css'
+import html from '../components/app-grid.html'
+
+export default
+
+module.exports = {
+  stories: ['../**/*.stories.js'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+}

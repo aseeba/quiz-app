d3ced9efa6aa56020591ef1164e9e072f9c63a43
Anja Seeba <ahoi@achtsam.media>
2021-01-27 10:44:20 +0100

    Adapt main.js and preview.js from Jerrys template

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,5 @@
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  stories: ['../**/*.stories.js'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 }

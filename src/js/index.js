91d3761dcca5bfba9c43de3635c23a315087d1ae
Anja Seeba <ahoi@achtsam.media>
2021-02-01 10:53:37 +0100

    remove code for form-counter

--- src/js/index.js
+++ src/js/index.js
@@ -83,37 +83,3 @@ cards.forEach(card => {
     toggleBookmark.classList.toggle('fas')
   })
 })
-
-//======== Zeichenlimit Formular ==========
-
-// const setQuestion = getElement('question')
-// const setAnswer = getElement('answer')
-// const setTags = getElement('tags')
-
-// function getElement(dataJsName) {
-//   return document.querySelector(`[data-js="${dataJsName}"]`)
-// }
-
-// const charLimit = getElement('charLimit')
-// setQuestion.addEventListener('input', () => {
-//   const maxChars = 100
-//   const charsUsed = setQuestion.nodeValue.length
-//   const charsLeft = maxChars - charsUsed
-//   charLimit.textContent = charsUsed + ' / ' + charsLeft
-// })
-
-// const charLimit = getElement('charLimit')
-// setAnswer.addEventListener('input', () => {
-//   const maxChars = 100
-//   const charsUsed = setQuestion.nodeValue.length
-//   const charsLeft = maxChars - charsUsed
-//   charLimit.textContent = charsUsed + ' / ' + charsLeft
-// })
-
-// const charLimit = getElement('charLimit')
-// setTags.addEventListener('input', () => {
-//   const maxChars = 75
-//   const charsUsed = setQuestion.nodeValue.length
-//   const charsLeft = maxChars - charsUsed
-//   charLimit.textContent = charsUsed + ' | ' + charsLeft
-// })
